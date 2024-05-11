e13a9cde467c85ea3d0053ab19181732a237c52a
mazharul-15 <mazharulislamhstu15@gmail.com>
2024-05-11 10:31:47 +0600

    API integrating

--- app/src/main/java/com/example/skysync/MainActivity.java
+++ app/src/main/java/com/example/skysync/MainActivity.java
@@ -7,6 +7,10 @@ import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.URL;
 
 import javax.net.ssl.HttpsURLConnection;
@@ -30,12 +34,43 @@ public class MainActivity extends AppCompatActivity {
 
    private class GetWeatherInfo extends AsyncTask<String, Void, String > {
 
-        HttpsURLConnection connection = null;
-
        @Override
        protected String doInBackground(String... urls) {
 
-           URL url = new URL(urls[0]);
+           HttpURLConnection connection = null;
+           BufferedReader reader = null;
+
+           try {
+
+               URL url = new URL(urls[0]);
+               connection = (HttpURLConnection) url.openConnection();
+               connection.connect();
+
+               reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+               StringBuilder buffer = new StringBuilder();
+               String line;
+
+               while((line = reader.readLine()) != null) {
+                   buffer.append(line).append("\n");
+               }
+
+               return buffer.toString();
+
+           } catch (IOException e) {
+                e.printStackTrace();
+           } finally {
+               if(connection != null) {
+                   connection.disconnect();
+               }
+
+               try {
+                   if(reader != null) {
+                       reader.close();
+                   }
+               } catch (IOException e) {
+                   e.printStackTrace();
+               }
+           }
 
            return null;
        }
